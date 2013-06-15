0e09947fdd75678e8818580d03b7619e5bf7df25
shane garven <shane.garven@gmail.com>
2013-06-15 20:29:20 +0800

    Adding WCFTest visual studio project and basic ICalculator service.
    Contains Add(), Subtract(), Multiple() and Divide().
    Initial solution and project commit

---
+++ WCFTest/CalculatorService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFTest
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CalculatorService" in both code and config file together.
+    public class CalculatorService : ICalculatorService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public double Add(int a, int b)
+        {
+            return (a + b);
+        }
+
+        public double Subtract(int a, int b)
+        {
+            return (a - b);
+        }
+
+        public double Multiply(int a, int b)
+        {
+            return (a*b);
+        }
+
+        /// <summary>
+        /// Returns the value of "a" divided by "b" (a / b).
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public double Divide(int a, int b)
+        {
+            return Convert.ToDouble((Convert.ToDouble(a)/Convert.ToDouble(b)));
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+    }
+}
---
+++ WCFTest/ICalculatorService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFTest
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICalculatorService" in both code and config file together.
+    [ServiceContract]
+    public interface ICalculatorService
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+        [OperationContract]
+        double Add(int a, int b);
+
+        [OperationContract]
+        double Subtract(int a, int b);
+
+        [OperationContract]
+        double Multiply(int a, int b);
+
+        [OperationContract]
+        double Divide(int a, int b);
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
